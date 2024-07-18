f716b88f1b2dcb154a24f3dd07d9445ebedbf71c
Breno Kleine Bernstein <87719398+brenokleine@users.noreply.github.com>
2024-07-18 11:05:53 -0300

    update irrelevant

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,13 @@
         <h1>
             Home Page
         </h1>
+        <div class="container">
+            <!-- <button @click.prevent="loginWithGoogle">Login with Google</button> -->
+        </div>
     </div>
 </template>
 
 <script setup>
+
 
 </script>
