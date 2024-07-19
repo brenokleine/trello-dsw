a4faa35bae28812a0d7aa5cbc3a15d6976c57a88
Breno Kleine Bernstein <87719398+brenokleine@users.noreply.github.com>
2024-07-19 13:49:20 -0300

    quadrosView pagina que necessita de estar logado para acessar

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
+import QuadrosView from '@/views/QuadrosView.vue'
+import { supabase } from '@/clients/supabase'
+
+let localUser;
 
 const router = createRouter({
   history: createWebHistory(),
@@ -15,7 +19,42 @@ const router = createRouter({
       name: 'login',
       component: LoginView
     },
+    {
+      path: '/quadros',
+      name: 'quadros',
+      component: QuadrosView,
+      meta: {
+        requiresAuth: true
+      }
+    }
   ]
+})
+
+
+//get user
+async function getUser(next){
+  localUser = await supabase.auth.getSession();
+
+  //se usuario tentar entrar em pagina que precisa de autenticacao e nao estiver logado, redireciona para login
+  if(localUser.data.session == null){
+   next('/login'); 
+  } else {
+    next();
+  }
+}
+
+
+//* auth verification -> verifica o auth toda vez q usuario mudar para pagina que precisa de autenticacao e chama o getUser
+// to -> para onde o usuario esta indo
+// from -> de onde o usuario esta vindo
+// next -> funcao que chama o proximo middleware
+
+router.beforeEach((to, from, next) => {
+  if(to.meta.requiresAuth){
+    getUser(next);
+  } else {
+    next();
+  }
 })
 
 export default router
---
+++ src/views/QuadrosView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        pagina de quadros
+    </div>
+</template>
+
+<script setup>
+
+</script>
